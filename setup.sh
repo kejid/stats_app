7b4c3bcba86c3afbdce6d9aa20ffa7429a4a8169
Kejid <36007786+kejid@users.noreply.github.com>
2020-06-10 10:20:07 +0300

    Update

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 mkdir -p ~/.streamlit/
-echo “\
+
+echo "\
 [general]\n\
-email = \”cratosartyom@gmail.com\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
+email = \"cratosartyom@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
